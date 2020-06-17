89b07faf229f32d6bb3166d470a0686716b653f7
Kent Schollmeier <jschollmeier@gmail.com>
2020-06-17 16:10:14 -0500

    close

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,45 @@
 $("#currentDay").text(moment().format('dddd, MMMM Do'));
+
+init();
+
+var times = []
+$(".form-control").each(function (){
+    times.push(this.id)
+});
+//var times2 = times.split(" ");
+
+
+for (var i = 0; i<times.length; i++){
+    if (times[i]===moment().format("H"))
+    {
+        $("#"+times[i]).css("background-color","red");
+        
+    }
+    else if (parseInt(times[i])>parseInt(moment().format("H"))){
+        $("#"+times[i]).css("background-color","green");
+        
+    }
+    else{
+        $("#"+times[i]).css("background-color","grey");
+        
+    }
+} 
+
+$(".btn").on('click', function(){
+    console.log($("#"+this.id).val());
+
+    localStorage.setItem(this.id, $("#"+this.id).val()) ;
+    
+})
+
+function init() {
+
+    for(var i = 9; i<18; i++)
+    {
+        $("#"+i).text(localStorage.getItem(i));
+    }
+
+  }
+  
+  
+
